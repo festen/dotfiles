9aeb7f1ac2553593db2cbc22d00ac8b20dddcc10
David Festen <d.festen@gmail.com>
2023-02-11 18:44:09 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,14 +1,6 @@
 set -v
 REPO_URL=${REPO_URL:-"git@github.com:festen/dotfiles.git"}
 
-while true; do
-    read -p "This will overwrite dotfiles in $HOME, continue? [yn]" yn
-    case $yn in
-        [Yy]* ) break;;
-        *) exit;;
-    esac
-done
-
 cd ~ || exit 1
 tmpdir=$(mktemp -d)
 git clone -c status.showUntrackedFiles=no -n --separate-git-dir "$HOME/.git" "$REPO_URL" "$tmpdir"
