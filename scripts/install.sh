1ebfc20dd09c656609935ecab40e9a307bafe96d
David Festen <d.festen@gmail.com>
2023-02-12 07:31:47 +0100

    v6

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env zsh
-RAW_URL="https://raw.githubusercontent.com/festen/dotfiles/v5"
+RAW_URL="https://raw.githubusercontent.com/festen/dotfiles/v6"
 
 function remoteExecute {
   source <(curl -s "$RAW_URL"/scripts/"$1")
 }
+
+sudo -v
 
 remoteExecute install-secrets.sh
 remoteExecute install-dotfiles.sh
 
 git checkout -f
 source "$HOME"/.zshrc
-nvm install default
-nvm alias default node
+nvm install --lts
+nvm alias default lts
 
 "$HOME"/scripts/install-brewfile.sh
 "$HOME"/scripts/install-tweaks.sh
