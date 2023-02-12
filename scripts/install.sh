ea70f447fba1bb7aec0ad1397af2540eac108d21
David Festen <d.festen@gmail.com>
2023-02-12 08:42:36 +0100

    v10

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env zsh
-set -eo pipefail
-
-RAW_URL="https://raw.githubusercontent.com/festen/dotfiles/v9"
+set -e
+sudo -v
+RAW_URL="https://raw.githubusercontent.com/festen/dotfiles/v10"
 
 function remoteExecute {
   source <(curl -s "$RAW_URL"/scripts/"$1")
 }
 
-sudo -v
-
 remoteExecute install-secrets.sh
 remoteExecute install-dotfiles.sh
-
-set +eo pipefail
 
 git checkout -f
 source "$HOME"/.zshrc
