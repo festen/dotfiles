83be89f3fe29423d4b509df0161d1a8c9a9257ce
David Festen <d.festen@gmail.com>
2023-02-12 06:00:57 +0100

    changed urls

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
-RAW_URL="https://raw.githubusercontent.com/festen/dotfiles/v3"
+RAW_URL="https://raw.githubusercontent.com/festen/dotfiles/v4"
 
 function remoteExecute {
   source <(curl -s "$RAW_URL"/scripts/"$1")
@@ -7,6 +7,9 @@ function remoteExecute {
 
 remoteExecute install-secrets.sh
 remoteExecute install-dotfiles.sh
+
 git checkout -f
-$HOME/scripts/install-brewfile.sh.sh
-$HOME/scripts/install-tweaks.sh
+source "$HOME"/.zshrc
+
+"$HOME"/scripts/install-brewfile.sh
+"$HOME"/scripts/install-tweaks.sh
