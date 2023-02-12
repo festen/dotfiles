8f9cb790767ebf331c86ae486c69fcd078311263
David Festen <d.festen@gmail.com>
2023-02-12 06:17:24 +0100

    updated urls again

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
-RAW_URL="https://raw.githubusercontent.com/festen/dotfiles/v4"
+RAW_URL="https://raw.githubusercontent.com/festen/dotfiles/v5"
 
 function remoteExecute {
   source <(curl -s "$RAW_URL"/scripts/"$1")
@@ -10,6 +10,8 @@ remoteExecute install-dotfiles.sh
 
 git checkout -f
 source "$HOME"/.zshrc
+nvm install default
+nvm alias default node
 
 "$HOME"/scripts/install-brewfile.sh
 "$HOME"/scripts/install-tweaks.sh
