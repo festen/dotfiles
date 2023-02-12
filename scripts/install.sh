d8e3d2faf0ac046e0161910359eed35958bba10d
David Festen <d.festen@gmail.com>
2023-02-12 09:38:41 +0100

    v12

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env zsh
 set -e
 sudo -v
-RAW_URL="https://raw.githubusercontent.com/festen/dotfiles/v11"
+RAW_URL="https://raw.githubusercontent.com/festen/dotfiles/v12"
 
 function remoteExecute {
-  source <(curl -s "$RAW_URL"/scripts/"$1")
+   curl -s "$RAW_URL"/scripts/"$1" | /usr/bin/env zsh
 }
 
 remoteExecute install-secrets.sh
