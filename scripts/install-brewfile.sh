0a7aa403f5f5d7b83b17ec669e81b4560e3db740
David Festen <d.festen@gmail.com>
2023-02-12 10:40:23 +0100

    v22

--- scripts/install-brewfile.sh
+++ scripts/install-brewfile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
-if ! command -v brew &> /dev/null; then
+if test $+commands[brew] -eq 0; then
     echo "Installing homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" </dev/null
 fi
-brew bundle --file="$HOME/scripts/Brewfile"
+brew bundle --file="$HOME/scripts/Brewfile" </dev/null
