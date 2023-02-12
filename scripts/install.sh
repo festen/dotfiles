9094bdffc18487ddcaa40f66c7027c1387fdf635
David Festen <d.festen@gmail.com>
2023-02-12 09:13:01 +0100

    v11

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 set -e
 sudo -v
-RAW_URL="https://raw.githubusercontent.com/festen/dotfiles/v10"
+RAW_URL="https://raw.githubusercontent.com/festen/dotfiles/v11"
 
 function remoteExecute {
   source <(curl -s "$RAW_URL"/scripts/"$1")
