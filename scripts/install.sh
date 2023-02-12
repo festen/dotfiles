d8bf946fbc0dcbb748b3cf02e8a8407f48bb7be3
David Festen <d.festen@gmail.com>
2023-02-12 11:12:35 +0100

    v25

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 set -e
 sudo -v
-RAW_URL="https://raw.githubusercontent.com/festen/dotfiles/v24"
+RAW_URL="https://raw.githubusercontent.com/festen/dotfiles/v25"
 
 function remoteExecute {
    curl -s "$RAW_URL"/scripts/"$1" | /usr/bin/env zsh
@@ -20,3 +20,5 @@ export NVM_DIR="${HOME}/.config/nvm"
 source "$(brew --prefix nvm)/nvm.sh"
 nvm install --lts
 nvm alias default node
+
+exec zsh
